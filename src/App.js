71aa37f741ee32a0c364f97d50e4592778bf9206
gourav jain <gouravjain2204@gmail.com>
2022-08-12 08:31:43 +0530

    changed the repository  name

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
   import React from 'react'
   import './App.css';
   import Textform from './components/Textform';
-  
- 
-  import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Greet from './components/Greet';
+  import Greet from './components/Greet';
 
 
   function App() {
