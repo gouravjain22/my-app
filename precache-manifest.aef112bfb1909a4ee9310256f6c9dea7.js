d278fa3c4592b3196ed623b429401c42bf3e2c75
gourav jain <gouravjain2204@gmail.com>
2022-08-14 10:00:38 +0530

    Updates

---
+++ precache-manifest.aef112bfb1909a4ee9310256f6c9dea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "adfe37fc6fe5aa9632cd",
+    "url": "/static/css/main.b9013768.chunk.css"
+  },
+  {
+    "revision": "adfe37fc6fe5aa9632cd",
+    "url": "/static/js/main.2c996104.chunk.js"
+  },
+  {
+    "revision": "bb1f08fe37d3c65590eb",
+    "url": "/static/js/runtime~main.e015284a.js"
+  },
+  {
+    "revision": "986a461ddad846ecc6f2",
+    "url": "/static/js/2.2ccfc8bd.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "5d099cd5766cc12c649e1a46a5581040",
+    "url": "/index.html"
+  }
+];
